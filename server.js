b24b11097da49bc11bc2f02afa9fa2931fc45b64
Matías <crosserain@gmail.com>
2023-09-16 14:22:45 -0300

    login route added

--- server.js
+++ server.js
@@ -1,18 +1,39 @@
 const express = require("express");
 const app = express();
-
+const bcrypt = require("bcrypt");
 app.use(express.json());
 
-const users = [{ name: "pablo", password: "admin" }];
+const users = [];
 
-app.get("/", (req, res) => {
+app.get("/users", (req, res) => {
     res.json(users);
 });
 
-app.post("/", (req, res) => {
-    const user = { name: req.body.nombre, password: req.body.password };
-    users.push(user);
-    res.status(201).send("User created");
+app.post("/users", async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = { name: req.body.name, password: hashedPassword };
+        users.push(user);
+        res.status(201).send("User created");
+    } catch {
+        res.status(500).send("Error");
+    }
+});
+
+app.post("/users/login", async (req, res) => {
+    const user = users.find((user) => user.name === req.body.name);
+    if (user === undefined) {
+        res.status(404).send("Cannot find user");
+    }
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            res.send("Loged in");
+        } else {
+            res.status(500).send("Not Allowed");
+        }
+    } catch {
+        res.status(500).send("Error");
+    }
 });
 
 app.listen(3000);
