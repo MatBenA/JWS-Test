ec4f1218a35d7acb7cb4f7f2d0e026073d599445
Matías <crosserain@gmail.com>
2023-09-17 22:08:52 -0300

    Authentication with JWT. Login sends web token

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 app.use(express.json());
 
 //User storage
@@ -12,7 +14,8 @@ app.get("/users", (req, res) => {
 });
 
 //create new user {name: ###, password: ###}
-app.post("/users", async (req, res) => { //bcrypt uses async await
+app.post("/users", async (req, res) => {
+    //bcrypt uses async await
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
         const user = { name: req.body.name, password: hashedPassword };
@@ -25,21 +28,29 @@ app.post("/users", async (req, res) => { //bcrypt uses async await
 
 //Login sistem
 app.post("/users/login", async (req, res) => {
-
     //Check if user exists, search by its name
-    const user = users.find((user) => user.name === req.body.name);     
+    const user = users.find((user) => user.name === req.body.name);
 
     if (user === undefined) {
         res.status(404).send("Cannot find user");
     } else {
         try {
+            
+            //Secure comparison that avoids timing attacks. If true user gets loged in
             if (await bcrypt.compare(req.body.password, user.password)) {
-                res.send("Loged in");
+                
+                //An access token that stores user info is created and sent
+                const accessToken = jwt.sign(
+                    user.name,
+                    process.env.ACCESS_TOKEN_SECRET
+                );
+                res.json({ accessToken });
             } else {
                 res.status(500).send("Not Allowed");
             }
-        } catch {
+        } catch (error) {
             res.status(500).send("Error");
+            throw error;
         }
     }
 });
